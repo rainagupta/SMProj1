ebd3a6c1c7fd799fc578157aac4659e60d365e39
rainagupta <47373755+rainagupta@users.noreply.github.com>
2020-02-04 15:18:57 -0500

    Add files via upload

---
+++ Date.java
@@ -0,0 +1,39 @@
+/**
+  
+ @author  Raina Gupta & Jay Mendapara
+ */
+public class Date 
+{
+   private int  day;
+   private int  month;
+   private int  year;
+   
+   public Date(String d)
+   {
+      //use StringTokenizer to parse the String and create a Date object     
+   }
+   
+   public Date(Date d)
+   {
+      //this is a constructor
+   }      
+   
+   public boolean isValid()
+   {
+       
+   }
+   
+   @Override
+   public String toString()
+   {
+       //use the format "month/day/year"
+   }
+   
+   @Override
+   public boolean equals(Object obj)
+   {
+       
+   }  
+}
+
+
